523d68f9d5412fb0fc62cdfdbc043ba17bec2880
Татьяна <atanae@mail.ru>
2023-09-09 17:07:10 +0300

    мое решение задачи13

---
+++ Less3.13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+123456789 -> 3*/
+
+string ThirdDigit()
+{
+    Console.Write("Введите любое число -> "); 
+    string textDigit = (Console.ReadLine());
+   
+    if (textDigit.Length < 3)
+        return("Третьей цифры нет");
+    else 
+        return("Третья цифра "+textDigit[2]);    
+}
+Console.WriteLine(ThirdDigit());
