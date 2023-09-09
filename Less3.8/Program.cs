a64ef3e5ea0b02f34d7b73657205ff540e7c35cc
Татьяна <atanae@mail.ru>
2023-09-09 18:13:50 +0300

    иправила табуляцию

--- Less3.8/Program.cs
+++ Less3.8/Program.cs
@@ -5,6 +5,6 @@ Console.WriteLine("Ведите число");
 int num = Convert.ToInt32(Console.ReadLine());
 int i = 2;
 while (i <= num)
-    {  Console.Write(i+" ");
+{   Console.Write(i+" ");
     i +=2;
-    }
+}
