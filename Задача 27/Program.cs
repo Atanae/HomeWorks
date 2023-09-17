0021c94af3b77c5f6a218bd28c59e65f5de181c0
Татьяна <atanae@mail.ru>
2023-09-17 20:26:35 +0300

    решение задачи 27

---
+++ Задача 27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+using System.Globalization;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int rezult = Convert.ToInt32(Console.ReadLine());
+    return rezult;
+}
+
+int summa(int numb)
+{   
+    int sum = 0;
+    while (numb > 0)
+    {
+        sum += numb % 10;
+        numb = numb / 10;  
+    }
+    return sum;
+}
+int number = Prompt("Введите число -> ");
+Console.WriteLine($"Сумма всех цифр числа "+number+" будет "+ summa(number));
+
